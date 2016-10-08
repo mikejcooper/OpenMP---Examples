5f351681ef10909d4c85d2770a2e839f018fbc68
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-08 14:46:22 +0100

    test

--- openmp/example3/numa-bad.c
+++ openmp/example3/numa-bad.c
@@ -24,7 +24,7 @@ int main (int argc, char *argv[]) {
   ** Access in parallel.
   */
   for(j=0; j<REPEAT; j++) {
-#pragma omp parallel for
+#pragma omp parallel for schedule(runtime) 
     for (i=0; i<N; i++) {
       a[i] = b[i];
     }
